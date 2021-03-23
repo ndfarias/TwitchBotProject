c5a423c98e087284b4af4cfd2222f9a1a7dd82d3
Nicol Farias <fariasnicol@yahoo.com>
2021-03-23 04:39:53 -0400

    message

--- app.js
+++ app.js
@@ -12,11 +12,16 @@ client.on('message', (channel, tags, message, self) => {
     // Ignore echoed messages.
     if(self) return;
   
+    //Add chat commands here !! 
     if(message.toLowerCase() === '!hello') {
       client.say(channel, `@${tags.username}, Hi there! Welcome to the stream! :)`);
     }
 
     if(message.toLowerCase() === '!swear') {
         client.say(channel, `@${tags.username}, Please do not swear on this channel! Thank you. `)
+    }
+
+    if(message.toLowerCase()== '!streamers') {
+        client.say(channel, `@${tags.username}, Please don't ask to see other streamers. It's considered rude.`)
     }
   });
