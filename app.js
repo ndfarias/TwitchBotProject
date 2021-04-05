b38194f9b7a348eb34f814efd0b41aefb8c5b1db
Nicol Farias <fariasnicol@yahoo.com>
2021-04-05 16:26:18 -0400

    add list of banned words

--- app.js
+++ app.js
@@ -7,14 +7,27 @@ const options = require("./options");
 const client = new tmi.client(options);
 client.connect();
 
-//Connect to api for uptime
+//Connect to api for stream length
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
 var request = new XMLHttpRequest();
 request.open("GET", "https://decapi.me/twitch/uptime?channel=nicnova_", true);
 
 //Connect to api for game info 
 var request2 = new XMLHttpRequest();
 request2.open("GET", "https://decapi.me/twitch/game/nicnova_", true);
+
+//put the txt file of forbidden words in an array
+var fs = require('fs');
+
+try {  
+    var data = fs.readFileSync('forbiddenwords.txt', 'utf8');
+
+    var forbiddenWords = data.split("\n");
+
+} catch(e) {
+    console.log('Error:', e.stack);
+}
 
 
 //Create and enable chat commands
@@ -28,7 +41,7 @@ client.on('message', (channel, tags, message, self) => {
     }
 
     if(message.toLowerCase() === '!swear') {
-        client.say(channel, `@${tags.username}, Please do not swear on this channel! Thank you. `);
+        client.say(channel, `@${tags.username}, Please do not swear or use hateful language on this channel! Thank you. `);
     }
 
     if(message.toLowerCase()== '!streamers') {
@@ -37,9 +50,12 @@ client.on('message', (channel, tags, message, self) => {
 
     //Code to delete messages - update with list of terms? 
 
-    if(message.toLowerCase().includes('fuck')){
-      client.deletemessage(channel, tags.id)
-      client.say(channel, `@${tags.username}, Please do not swear on this channel! Thank you. `);
+    for (var i = 0; i < forbiddenWords.length; i++) {
+      if(message.toLowerCase().includes(forbiddenWords[i])){
+        client.deletemessage(channel, tags.id)
+        client.say(channel, `@${tags.username}, Please do not swear or use hateful language on this channel! Thank you. `);
+        break;
+      }
     }
 
     //can only start polls as twitch partner/affiliate - cannot test 
